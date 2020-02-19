e0fe7f80f760838ca68136332425b85aca3687c2
vsh123 <vsh123@naver.com>
2020-02-19 18:51:31 +0900

    A

--- git-hooks/setup.sh
+++ git-hooks/setup.sh
@@ -2,3 +2,8 @@
 
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 rm ${PROJECT_ROOT}/.git/hooks/pre-push || true; ln -s ${PROJECT_ROOT}/git-hooks/pre-push ${PROJECT_ROOT}/.git/hooks/pre-push
+
+
+
+
+
